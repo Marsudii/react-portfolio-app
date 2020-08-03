1397ece4740762afbc2fc108e531d226e86f1ea8
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2020-08-03 16:35:44 +0300

    add sitemap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
 import App from './views/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<BrowserRouter>
@@ -10,3 +11,5 @@ ReactDOM.render(
 
 	document.getElementById('root')
 );
+
+serviceWorker.unregister();
