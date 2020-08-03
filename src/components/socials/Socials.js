288e80ed65b538eef94f5da6686e3512f654796d
Alexander Tsiomakh <a.tsiomakh@gmail.com>
2020-08-03 18:20:09 +0300

    add icons font instead svg

--- src/components/socials/Socials.js
+++ src/components/socials/Socials.js
@@ -1,11 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import GitHubIco from './icons/github.svg';
-import PhoneIco from './icons/phone.svg';
-import TelegaIco from './icons/telegram.svg';
-import CVIco from './icons/cv.svg';
-
 import './Socials.scss';
 
 function Socials() {
@@ -13,19 +8,19 @@ function Socials() {
 		<div className="contact-menu d-flex-c">
 			<Link to={location => ({ ...location, pathname: "https://github.com/buhowski" })} target="_blank">
 				<span className="title">GitHub</span>
-				<span className="icon" style={{backgroundImage: `url(${GitHubIco})`}}></span>
+				<span className="icon-github"></span>
 			</Link>
 			<a href="tel:+380993129768">
-					<span className="title">Call</span>
-					<span className="icon" style={{backgroundImage: `url(${PhoneIco})`}}></span>
+				<span className="title">Call</span>
+				<span className="icon-phone"></span>
 			</a>
 			<Link to={location => ({ ...location, pathname: "https://t.me/buhowski" })} target="_blank">
 				<span className="title">Telegram</span>
-				<span className="icon" style={{backgroundImage: `url(${TelegaIco})`}}></span>
+				<span className="icon-telegram"></span>
 			</Link>
-			<Link to={location => ({ ...location, pathname: "https://drive.google.com/file/d/1stKbNmYwRmIJxBdVlsGyeGpjRjuIhPr0/view?usp=sharing" })} target="_blank">
+			<Link to={location => ({ ...location, pathname: "https://drive.google.com/file/d/1Q15mgFV3ZLaNCjAWv8tbeXNkQKr3UyIb/view?usp=sharing" })} target="_blank">
 				<span className="title">CV</span>
-				<span className="icon" style={{backgroundImage: `url(${CVIco})`}}></span>
+				<span className="icon-cv"></span>
 			</Link>
 		</div>
 	)
